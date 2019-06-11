0a71f8268af7e0cf8e4775466aa03f15cbc67e1a
Sapir Hirak <sapirosh1h@gmail.com>
2019-06-11 14:53:29 +0300

    first commit

---
+++ day2.js
@@ -0,0 +1,13 @@
+const maxCapacity = 14
+console.log("hello")
+let surfTime = true
+let bestStudent
+let str = 'daniel'
+let result =str + 'is the best thing ever'
+console.log(result)
+console.log(str === result)
+console.log(123*2)
+console.log(802/2)
+console.log((5+6)*3)
+console.log(undefined || null)
+git 
